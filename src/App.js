4ec4dd2b618ebfee803a59c4073f99aba7291869
Matt Pusch <mpusch88@gmail.com>
2023-04-05 21:00:05 -0700

    Updated App.js and added NotFoundPage

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/Authentication/LoginPage";
+import NotFoundPage from "./pages/NotFoundPage";
 import Navbar from "./components/Navbar";
 
-function App() {
+function useAuthentication() {
   const [authenticated, setAuthenticated] = useState(false);
 
-  const handleLogin = useCallback(() => {
+  const login = useCallback(() => {
     setAuthenticated(true);
   }, []);
 
-  const handleLogout = useCallback(() => {
+  const logout = useCallback(() => {
     setAuthenticated(false);
   }, []);
 
+  // Handle user authentication persistence
+  useEffect(() => {
+    const storedAuth = localStorage.getItem("authenticated");
+    if (storedAuth) {
+      setAuthenticated(JSON.parse(storedAuth));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("authenticated", JSON.stringify(authenticated));
+  }, [authenticated]);
+
+  return { authenticated, login, logout };
+}
+
+function App() {
+  const { authenticated, login, logout } = useAuthentication();
+
   return (
     <Router>
-      <Navbar authenticated={authenticated} onLogout={handleLogout} />
+      <Navbar authenticated={authenticated} onLogout={logout} />
       <Routes>
         <Route path="/" element={authenticated ? <HomePage /> : <Navigate to="/login" />} />
-        <Route path="/login" element={<LoginPage onLogin={handleLogin} />} />
+        <Route path="/login" element={<LoginPage onLogin={login} />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </Router>
   );
